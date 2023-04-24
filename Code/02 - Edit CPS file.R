b646d620440262acf34a8c508b4ddfcbe1e77502
Schubert <schubert@demogr.mpg.de>
2023-04-24 17:26:39 +0200

    Update the CPS code.

--- Code/02 - Edit CPS file.R
+++ Code/02 - Edit CPS file.R
@@ -9,7 +9,7 @@
   # Edited on: 21.04.2022
 
 
-### Preparations -------------------------------------
+### Preparations ------------------------------------------------
 
   # Clear the environment
   rm(list = ls())
@@ -22,26 +22,27 @@
   
   
   # Load the data
-  d <- fread("Raw_data/cps_00003.csv")
+  d <- fread("Raw/cps_00003.csv", integer64 = "numeric")
   
   # Years
   years <- 1969:2021
+  ages  <- 15:50
 
-### Recoding variablels -----------------------------------------
+### Recoding variables -----------------------------------------
 
 
   # Make names small
   names(d) <- str_to_lower(names(d))
   
   # Filter the years and women
-  d <- d[year %in% years & sex == 2 & month == 6] 
+  d <- d[year %in% years & sex == 2 & month == 6 & age %in% ages] 
   
   # Create missing values
   d$frever[d$frever == 999] <- NA # Number of live births ever had
-  d$frage1[d$frage1 == 999] <- NA # Women's age in months at birth of first child
-  d$frage2[d$frage2 == 999] <- NA # Women's age in months at birth of second child
-  d$frage3[d$frage3 == 999] <- NA # Women's age in months at birth of third child
-  d$frage4[d$frage4 == 9999] <- NA # Women's age in months at birth of fourth child
+  d$frage1[d$frage1 == 999] <- NA # Woman's age in months at birth of first child
+  d$frage2[d$frage2 == 999] <- NA # Woman's age in months at birth of second child
+  d$frage3[d$frage3 == 999] <- NA # Woman's age in months at birth of third child
+  d$frage4[d$frage4 == 9999] <- NA # Woman's age in months at birth of fourth child
   d$frbirthy1[d$frbirthy1 == 9999] <- NA # Birth year of first child
   d$frbirthy2[d$frbirthy2 == 9999] <- NA # Birth year of second child
   d$frbirthy3[d$frbirthy3 == 9999] <- NA # Birth year of third child
@@ -65,13 +66,16 @@
   d <- d %>% select(cpsid, parity, frever, year, sex, age, race, wtfinl) 
   
 ### Load the background information --------------------------------
-    
 
   # Load the normal data
-  d2 <- fread("Raw_data/cps_19902019.csv")
+  d2 <- fread("Raw/cps_19902019.csv", integer64 = "numeric")
   
   # Select the variables
-  d2 <- d2[ ,.(cpsid, year, serial,  sex, age,  educ, asecwt)]
+  d2 <- d2[ ,.(cpsid, year, serial,  sex, age,  educ, asecwt, hispan)]
+  
+  # Replace hispanic
+  d2$hispanic <- ifelse(d2$hispan == "Do not know", NA_character_,
+                        ifelse(d2$hispan == "Not Hispanic", "Non-Hispanic", "Hispanic"))
   
   # Change the age variable
   d2$age <- as.numeric(d2$age)
@@ -79,54 +83,102 @@
   
 ### Combine the fertility supplement with the basic data -----------------
   
-  
   # Merge the files
-  d3 <- left_join(d, d2, by = c("year", "cpsid", "age"), suffix = c("", "_fertility"))
+  d3 <- inner_join(as_tibble(d), d2, by = c("year", "cpsid"), suffix = c("", "_fertility"))
   
   # Filter the variables
-  d3 <- d3 %>% filter(age >= 15 & age <= 49 & sex == 2) 
+  d3 <- d3 %>% filter(age %in% ages & sex == 2) 
   
   # Recode age, weight, sex and educatin variable
   d3 <- d3 %>% rename(weight = asecwt)
-  
-  # Create a tibble
-  d3 <- as_tibble(d3)
   
   # Save the data
   save(d3, file = "Data/cps_complete.Rda")
   
   
-### Clean the data ------------------------------------------------------
+### Clean the educational data -------------------------------------------
+  
+  # Subset women in the fertility survey
+  d <- subset(d3, !is.na(parity))
+  
+  # Create educational classes
+  educ1 <- c("None or preschool", "Grades 1, 2, 3, or 4", "Grades 5 or 6", "Grades 7 or 8", paste("Grade", 1:11), "12th grade, no diploma")
+  educ2 <- c("12th grade, diploma unclear",  "High school diploma or equivalent")
+  educ3 <- c("Professional school degree", "1 year of college", paste(2:5, "years of college"), "Some college but no degree")
+  educ4 <- c("6+ years of college", "Associate's degree, academic program", "Associate's degree, occupational/vocational program","Bachelor's degree",  "Master's degree", "Doctorate degree")
   
   # Make education variable
+  d$education <- NA
+  d[d$educ %in% educ1, ]$education <- "No high school degree"
+  d[d$educ %in% educ2, ]$education <- "High school diploma"
+  d[d$educ %in% educ3, ]$education <- "Some college"
+  d[d$educ %in% educ4, ]$education <- "College degree"
+  d[d$educ == "NIU or blank", ]$education    <- NA
+  
+  # Make educatin a factor variable
+  d$education <- factor(d$education, levels = c("No high school degree", "High school diploma", "Some college", "College degree"))
+  
+### Clean the educational data -------------------------------------------
   
   # Create age-groups
-  age_group = cut(age, breaks = seq(15, 49, by = 5))
+  d$age_group <- cut(d$age, breaks = seq(15, 50, by = 5), include.lowest = T, right = F)
   
 ### Plot the population structure ---------------------------------------
 
-  # Calculate the population share
-  distribution <- d3 %>% group_by(year, age_group) %>% mutate(Total = n()) %>% 
-    group_by(age_group, education, race, year, parity) %>% summarise(Proportion = n()/Total) 
+  # Plot the change in the age structure
+  ggplot(d, aes(year,  fill = age_group)) +
+    geom_histogram(aes(weight = wtfinl)) +
+    ylab(NULL) + 
+    scale_fill_viridis_d() +
+    scale_y_continuous(labels = unit_format(unit = "M", scale = 1e-6), expand = c(0, 0))
   
   
-  # Complete cases
-   Distribution <- distribution %>% 
-   complete(age_group, education, race, year, parity, fill = list(Proportion = 0))
+  # Plot the change in the educational structure
+  ggplot(d, aes(year,  fill = education)) +
+    geom_histogram(aes(weight = wtfinl)) +
+    ylab(NULL) + 
+    scale_fill_viridis_d() +
+    scale_y_continuous(labels = unit_format(unit = "M", scale = 1e-6), expand = c(0, 0))
+  
+  # Hispanic
+  ggplot(d, aes(year,  fill = hispanic)) +
+    geom_histogram(aes(weight = wtfinl)) +
+    ylab(NULL) + 
+    scale_fill_viridis_d() +
+    scale_y_continuous(labels = unit_format(unit = "M", scale = 1e-6), expand = c(0, 0))
 
-  # Plot the population structure
-  ggplot(distribution, aes(x = age_group, y = Proportion, group = education, fill = education)) +
-    geom_col() +
-    facet_wrap(year ~ race)
+  # Race
+  ggplot(d, aes(year,  fill = race)) +
+    geom_histogram(aes(weight = wtfinl)) +
+    ylab(NULL) + 
+    scale_fill_viridis_d() +
+    scale_y_continuous(labels = unit_format(unit = "M", scale = 1e-6),
+                       expand = c(0, 0))
   
-  ggplot(distribution, aes(x = year, y = parity, fill = Proportion)) +
-    geom_tile()+
-    facet_grid(age_group ~ race + education) + 
-    theme(legend.position = "bottom") +
-    scale_fill_viridis_c(option = "H")
+  # Parity distribution
+  ggplot(d, aes(year,  fill = as.factor(parity))) +
+    geom_histogram(aes(weight = wtfinl)) +
+    ylab(NULL) + 
+    scale_fill_viridis_d() +
+    scale_y_continuous(labels = unit_format(unit = "M", scale = 1e-6), expand = c(0, 0))
+
+  # Conditional distribution of age and parity
+  d %>% group_by(age_group, parity) %>% summarise(share = sum(wtfinl)) %>% 
+    ggplot(aes(age_group, parity, fill = share)) + 
+    geom_tile() +
+    theme(legend.key.width = unit(3, "cm")) +
+    scale_fill_viridis_c()
   
-  ggplot(distribution, aes(x = year, y = Proportion, group = education, colour = education)) +
-    geom_line() +
-    geom_point() +
-    facet_grid(age_group ~ race + education) + 
-    theme(legend.position = "bottom")
+  
+### Estimate the exposures -----------------------------------------
+  
+  
+  # Estimate population count
+  exposure <- d %>% group_by(age_group, parity, year, hispanic, race) %>% 
+    summarise(Pop = sum(wtfinl))
+  
+  
+  # Save the exposure data
+  save(exposure, file = "Data/exosure.Rda")
+  
+##########        END           ###########
